1d39e63432f6979b8bd6a38c23293b8f1946471f
Lindapig <lindaz888666@gmail.com>
2025-04-21 01:46:24 -0400

    deploy configuration

--- api/src/routes/auth.js
+++ api/src/routes/auth.js
@@ -118,10 +118,13 @@ router.post("/login", async (req, res) => {
     const payload = { userId: user.user_id };
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "15m" });
 
+    const isProduction = process.env.NODE_ENV === "production";
+
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: 'None'
+      sameSite: isProduction ? "None" : "Lax",
+      secure: isProduction
     });
 
     res.json({
